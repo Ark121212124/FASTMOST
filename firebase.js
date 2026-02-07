96336edec4b701f95213662af413f1d04019dff3
Ark121212124 <Arktiko12@gmail.com>
2026-02-07 22:49:20 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+<!-- firebase.js -->
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "ТВОЙ_API_KEY",
+  authDomain: "fastmost-f8084.firebaseapp.com",
+  projectId: "fastmost-f8084",
+  storageBucket: "fastmost-f8084.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+</script>
