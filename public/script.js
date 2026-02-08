7371af1ed752b6620ace75c8c8ae86f5d4c94a32
Ark121212124 <Arktiko12@gmail.com>
2026-02-08 15:39:48 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,31 +5,21 @@ const ws = new WebSocket(
 const messages = document.getElementById("messages");
 const username = localStorage.getItem("username") || "Guest";
 
+let currentChannel = "общий";
+
 ws.onmessage = e => {
   const data = JSON.parse(e.data);
 
   if (data.type === "online") {
-    document.getElementById("online").textContent =
-      "🟢 Онлайн: " + data.count;
+    online.textContent = "🟢 Онлайн: " + data.count;
     return;
   }
 
-  if (data.type === "file") {
-    const div = document.createElement("div");
-    div.className = "message";
-    div.innerHTML = `
-      <span class="user">${data.user}</span>
-      <span class="time">${data.time}</span>
-      <div class="text">
-        <a href="${data.url}" target="_blank">${data.name}</a>
-      </div>
-    `;
-    messages.appendChild(div);
-    return;
-  }
+  if (data.channel !== currentChannel) return;
 
   const msg = document.createElement("div");
   msg.className = "message";
+
   msg.innerHTML = `
     <span class="user">${data.user}</span>
     <span class="time">${data.time}</span>
@@ -45,10 +35,31 @@ function send() {
 
   ws.send(JSON.stringify({
     type: "message",
+    channel: currentChannel,
     user: username,
     text: msg.value,
     time: new Date().toLocaleTimeString().slice(0,5)
   }));
 
   msg.value = "";
+}
+
+/* CHANNELS */
+const textChannels = document.getElementById("textChannels");
+
+function createChannel() {
+  const name = prompt("Название канала");
+  if (!name) return;
+
+  const div = document.createElement("div");
+  div.className = "channel";
+  div.textContent = "# " + name;
+  div.onclick = () => switchChannel(name);
+  textChannels.appendChild(div);
+}
+
+function switchChannel(name) {
+  currentChannel = name;
+  channelName.textContent = "# " + name;
+  messages.innerHTML = "";
 }
