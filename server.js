98b0a47ac2697a2eeaf610352aac716eb5c979d7
Ark121212124 <Arktiko12@gmail.com>
2026-02-21 13:23:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,114 +1,17 @@
-const http=require("http");
-const fs=require("fs");
-const path=require("path");
-const WebSocket=require("ws");
-const crypto=require("crypto");
-
-
-const PUBLIC =
-path.join(__dirname,"public");
-
-
-/* HTTP */
-const server=http.createServer((req,res)=>{
-
- const file =
- req.url === "/"
- ? "/index.html"
- : req.url;
-
- const filePath =
- path.join(PUBLIC,file);
-
-
- fs.readFile(filePath,(err,data)=>{
-
-  if(err){
-
-   res.writeHead(404);
-
-   return res.end();
-
-  }
-
-
-  const types={
-
-   ".html":"text/html",
-
-   ".css":"text/css",
-
-   ".js":"application/javascript",
-
-   ".svg":"image/svg+xml"
-
-  };
-
-
-  res.writeHead(200,{
-
-   "Content-Type":
-   types[path.extname(filePath)]
-   || "text/plain"
-
-  });
-
-
-  res.end(data);
-
- });
-
-});
-
-
-/* WEBSOCKET */
-const wss =
-new WebSocket.Server({server});
-
-
-const clients =
-new Map();
-
-
-function send(ws,data){
-
- if(ws.readyState === 1)
- ws.send(JSON.stringify(data));
-
-}
-
+const voiceRooms = {};
 
 function broadcastVoice(channel){
 
- const users=[];
+ const users =
+ voiceRooms[channel] || [];
 
- for(const [id,c] of clients){
+ for(const ws of clients.values()){
 
-  if(c.voice === channel){
+  if(ws.voice === channel){
 
-   users.push({
-
-    id,
-
-    username:c.username
-
-   });
-
-  }
-
- }
-
-
- for(const c of clients.values()){
-
-  if(c.voice === channel){
-
-   send(c,{
-
+   send(ws,{
     type:"voice-users",
-
     users
-
    });
 
   }
@@ -118,113 +21,40 @@ function broadcastVoice(channel){
 }
 
 
-wss.on("connection",ws=>{
+if(d.type === "voice-join"){
 
- ws.id =
- crypto.randomUUID();
+ ws.voice = d.channel;
 
- ws.username =
- "Guest";
+ ws.username = d.user;
 
- ws.voice =
- null;
+ if(!voiceRooms[d.channel])
+ voiceRooms[d.channel] = [];
 
-
- clients.set(ws.id,ws);
-
-
- send(ws,{
-
-  type:"init",
-
-  id:ws.id
-
+ voiceRooms[d.channel]
+ .push({
+  id:ws.id,
+  username:ws.username
  });
 
+ broadcastVoice(d.channel);
 
- ws.on("message",raw=>{
-
-  const d =
-  JSON.parse(raw);
+}
 
 
-  if(d.type === "voice-join"){
+if(d.type === "voice-leave"){
 
-   ws.voice =
-   d.channel;
+ const room =
+ voiceRooms[ws.voice];
 
-   ws.username =
-   d.user || "Guest";
+ if(room){
 
+  voiceRooms[ws.voice] =
+  room.filter(u => u.id !== ws.id);
 
-   broadcastVoice(ws.voice);
+ }
 
-  }
+ broadcastVoice(ws.voice);
 
+ ws.voice = null;
 
-  if(d.type === "voice-leave"){
-
-   const old =
-   ws.voice;
-
-   ws.voice =
-   null;
-
-   if(old)
-   broadcastVoice(old);
-
-  }
-
-
-  if(
-
-   d.type === "voice-offer" ||
-
-   d.type === "voice-answer" ||
-
-   d.type === "voice-ice"
-
-  ){
-
-   const to =
-   clients.get(d.to);
-
-   if(to){
-
-    send(to,{
-
-     ...d,
-
-     from:ws.id
-
-    });
-
-   }
-
-  }
-
- });
-
-
- ws.on("close",()=>{
-
-  const old =
-  ws.voice;
-
-  clients.delete(ws.id);
-
-  if(old)
-  broadcastVoice(old);
-
- });
-
-});
-
-
-server.listen(
-
- process.env.PORT || 10000,
-
- ()=>console.log("🚀 FASTMOST Voice Ready")
-
-);
+}
